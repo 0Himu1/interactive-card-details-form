75b40ab1b5f4251b9090413977ec9111514d9da9
0himu1 <0himu1@gmail.com>
2023-01-09 23:59:23 +0600

    day two, need more time on css

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,32 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import './Styles/Form.css';
 
 export default function Form() {
   return (
-    <div>Form</div>
+    <form>
+      <div className="input">
+        <label htmlFor="name">CARDHOLDER NAME</label>
+        <input type="text" />
+      </div>
+      <div className="input">
+        <label htmlFor="cardNumber">CARD NUMBER</label>
+        <input type="text" />
+      </div>
+      <div className="split">
+        <div className="input">
+          <label htmlFor="name">EXP. DATE (MM/YY)</label>
+          <div className="split">
+            <input type="text" />
+            <input type="text" />
+          </div>
+        </div>
+        <div className="input">
+          <label htmlFor="cardNumber">CVC</label>
+          <input type="text" />
+        </div>
+      </div>
+      <button type="submit">Confirm</button>
+    </form>
   );
 }
